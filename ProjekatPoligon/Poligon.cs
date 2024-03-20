f5c3d7cd98339107e466c9eccbfb89fbc1c35714
Marko Petrović <petrovicmarko2006@gmail.com>
2024-03-20 14:10:27 +0100

    Initial commit

---
+++ ProjekatPoligon/Poligon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Poligon
+{
+	List<Tacka> Temena;
+
+	public Poligon(List<Tacka> Niz_temena) => Temena = Niz_temena;
+	public Poligon() => Temena = new List<Tacka>();
+	public Poligon(params Tacka[] Temena) => this.Temena = new List<Tacka>(Temena);
+	public void DodajTeme(Tacka A) => Temena.Add(A);
+	public double Povrsina()
+	{
+		if (!Prost())
+			return -1;
+		return 0;
+	}
+	public double Obim()
+	{
+		// Domaci 3 - uradi obim
+		return 0;
+	}
+	public void Stampaj()
+	{
+		// Domaci 4 - stampaj sva temena
+	}
+	public bool Konveksan()
+	{
+		// Domaci 5 – Proveri da li je konveksan
+		return false;
+	}
+	public bool Prost()
+	{
+		// Domaci 6 - proveri da li je prost (nema ponovljenih temena i stranice se ne seku)
+		return false;
+	}
+	public Poligon Omotac()
+	{
+		// Domaci 7 - formiraj konveksni omotac
+		return new Poligon();
+	}
+	public void Snimi()
+	{
+		try {
+			BinaryFormatter formatter = new BinaryFormatter();
+			Console.Write("Enter the file name: ");
+			using (FileStream stream = new FileStream(Console.ReadLine(), FileMode.Create)) {
+				formatter.Serialize(stream, Temena);
+			}
+		} catch (Exception ex) {
+			Console.Error.WriteLine("Unable to save the polygon.");
+			Console.Error.WriteLine($"Error: {ex.Message}");
+		}
+		
+	}
+	public void Ucitaj()
+	{
+		try {
+			BinaryFormatter formatter = new BinaryFormatter();
+			Console.Write("Enter the file name: ");
+			using (FileStream stream = new FileStream(Console.ReadLine(), FileMode.Open)) {
+				Temena = (List<Tacka>)formatter.Deserialize(stream);
+			}
+		} catch (Exception ex) {
+			Console.Error.WriteLine("Unable to load the polygon.");
+			Console.Error.WriteLine($"Error: {ex.Message}");
+		}
+	}
+}
---
+++ ProjekatPoligon/Vektor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Tacka
+{
+	public double x, y;
+	public Tacka(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
+public class Vektor
+{
+	public double x, y;
+	public Vektor(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	public Vektor(Tacka A, Tacka B)
+	{
+		this.x = B.x - A.x;
+		this.y = B.y - A.y;
+	}
+	public static double Skalarni(Vektor A, Vektor B) => A.x * B.x + A.y * B.y;
+	public static double Vektorski(Vektor A, Vektor B) => A.x * B.y - A.y * B.x;
+}
