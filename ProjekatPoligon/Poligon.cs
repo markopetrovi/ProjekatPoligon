41a1814db26022a86d19bd9c79d0faa7db02cd26
Marko Petrović <petrovicmarko2006@gmail.com>
2024-03-22 13:04:17 +0100

    Commit

--- ProjekatPoligon/Poligon.cs
+++ ProjekatPoligon/Poligon.cs
@@ -15,26 +15,76 @@ public class Poligon
 	{
 		if (!Prost())
 			return -1;
-		return 0;
+		double glavna = 0, sporedna = 0;
+		for (int i = 0; i < Temena.Count - 1; i++) {
+			glavna += Temena[i].x * Temena[i + 1].y;
+			sporedna += Temena[i].y * Temena[i + 1].x;
+		}
+		glavna += Temena[Temena.Count - 1].x * Temena[0].y;
+		sporedna += Temena[Temena.Count - 1].y * Temena[0].x;
+		return Math.Abs(glavna - sporedna) / 2;
 	}
 	public double Obim()
 	{
-		// Domaci 3 - uradi obim
-		return 0;
+		if (Temena.Count < 2) return 0;
+		Tacka prev = Temena[0];
+		double duzina = 0;
+		for (int i = 1; i < Temena.Count; i++) {
+			duzina += new Vektor(prev, Temena[i]).Duzina();
+			prev = Temena[i];
+		}
+		return duzina;
 	}
 	public void Stampaj()
 	{
-		// Domaci 4 - stampaj sva temena
+		for (int i = 0; i < Temena.Count; i++)
+			Console.WriteLine($"A{i}: ({Temena[i].x}, {Temena[i].y})");
 	}
 	public bool Konveksan()
 	{
-		// Domaci 5 – Proveri da li je konveksan
-		return false;
+		if (!Prost())
+			return false;
+		List<Tacka> l = new List<Tacka>();
+		foreach (Tacka T in Temena) { l.Add(T); }
+		l.Add(l[0]);
+		int k = 0;
+		for (int i = 0; i < Temena.Count - 1; i++) {
+			Vektor A = new Vektor(l[i], l[i + 1]);
+			Vektor B = new Vektor(l[i + 1], l[i + 2]);
+			if (Vektor.Vektorski(A, B) * k < 0)
+				return false;
+			if (k == 0)
+				k = Math.Sign(Vektor.Vektorski(A, B));
+		}
+		return true;
 	}
 	public bool Prost()
 	{
 		// Domaci 6 - proveri da li je prost (nema ponovljenih temena i stranice se ne seku)
-		return false;
+		if (Temena.Count < 2)
+			return false;
+		HashSet<Tacka> set = new HashSet<Tacka>();
+		foreach(Tacka tacka in Temena) {
+			if (set.Contains(tacka))
+				return false;
+			set.Add(tacka);
+		}
+		if (Temena.Count == 3)
+			return Vektor.Vektorski(new Vektor(Temena[0], Temena[1]), new Vektor(Temena[0], Temena[2])) != 0;
+
+		Tacka prev = Temena[0];
+		for (int i = 1; i < Temena.Count; i++) {
+			Tacka previous = Temena[i+1];
+			Vektor osnova = new Vektor(prev, Temena[i]);
+			for (int p = i+2; p < Temena.Count; p++) {
+				if (osnova.SIS(previous, Temena[p]))
+					return false;
+				previous = Temena[p];
+			}
+			prev = Temena[i];
+		}
+		
+		return true;
 	}
 	public Poligon Omotac()
 	{
--- ProjekatPoligon/Vektor.cs
+++ ProjekatPoligon/Vektor.cs
@@ -12,6 +12,7 @@ public class Tacka
 public class Vektor
 {
 	public double x, y;
+	public Tacka A, B;
 	public Vektor(double x, double y)
 	{
 		this.x = x;
@@ -21,7 +22,11 @@ public class Vektor
 	{
 		this.x = B.x - A.x;
 		this.y = B.y - A.y;
+		this.A = A;
+		this.B = B;
 	}
 	public static double Skalarni(Vektor A, Vektor B) => A.x * B.x + A.y * B.y;
 	public static double Vektorski(Vektor A, Vektor B) => A.x * B.y - A.y * B.x;
+	public bool SIS(Tacka A, Tacka B) => (Vektorski(this, new Vektor(this.B, A)) * Vektorski(this, new Vektor(this.B, B))) >= 0;
+	public double Duzina() => Math.Sqrt(x*x + y*y);
 }
